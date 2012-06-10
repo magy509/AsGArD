6d1ee253c99bae8655b1a5b75dbfbc692ba1f471
Christian Chomiak <christianchomiak@gmail.com>
2012-06-10 19:17:09 -0430

    Yey D:

--- Main.hs
+++ Main.hs
@@ -3,7 +3,30 @@ module Main (main) where
 import Language.AsGArD.Lexer (alexScanTokens)
 import Language.AsGArD.Parser (parse)
 
-main = print . parse . alexScanTokens =<< getContents
+import Language.AsGArD.Lexer.Token
+
+--main = print . parse . alexScanTokens =<< getContents
+
+printElements :: [Token] -> IO()
+printElements [] = return ()
+printElements (x:xs) = do print x
+                          printElements xs
+
+
+main = do
+
+   s <- getContents
+   let x = (alexScanTokens s)
+   let errores = filter (\ x -> case x of
+         TkError _ _ _ -> True
+         otherwise -> False) x
+   if ((length errores) == 0) then 
+      --(printElements x)
+      print (parse x)
+   else 
+      (printElements errores)
+   --printElements x
+
 
 -- Algo sobre el main que no quiero hacer T_T
 -- main = do
