b63fb1f22dc850d4f3e3ef4c6afa1a92a6aaae86
María Gracia Hidalgo <magy509@gmail.com>
2012-07-04 18:50:45 -0430

    CI3725 AJ2012 - Etapa 3

---
+++ StaticAnalyzer.hs
@@ -0,0 +1,86 @@
+--  Module StaticAnalyzer
+--  Universidad Simón Bolívar
+--  Laboratorio de Traductores e Interpretadores (CI-3725)
+--  Proyecto 3 - Análisis de Contexto e Intérprete
+--  Desarrollado por: Christian Chomiak     05-38034
+--                    Maria Gracia Hidalgo  03-36048
+
+module StaticAnalyzer ()
+       where
+
+import Control.Monad
+import Control.Monad.Instances
+import Control.Monad.State
+import qualified Data.List as List
+import qualified Data.Map as Map
+import Language.AsGArD.Parser.AST
+import Language.AsGArD.Lexer.Token
+import SymTable
+import System.IO
+
+
+{-checkDec::[Declaracion]->SymTable->Bool
+checkDec ((Declaracion (x: xs) tipo):ds) st
+    =  if isMember nombre st then
+        if xs /= [] then
+           checkDec [Declaracion xs tipo] st
+        else
+           if ds /= [] then
+              checkDec ds st
+        else
+              True
+      else
+           error $ "Variable "++ show x ++" no declarada"
+      where
+        nombre = show x
+  
+  -}
+  
+checkDec::Declaracion->SymTable->Bool
+checkDec (Declaracion (x:xs) tipo) st
+    = if isMember nombre st then
+         if xs == [] then
+            True
+         else
+            checkDec (Declaracion xs tipo) st
+      else
+         error $ "Variable " ++ show x ++ " no declarada."
+      where
+         nombre = show x
+  
+checkEst::Token -> SymTable -> Declaracion -> Bool
+checkEst nombre st (Declaracion listanombres tipo)
+    = if (elem nombre listanombres && tipo == dtipo) then
+        True
+      else
+         error $ "Error de tipo."
+    where
+        snombre = show nombre
+        Just (SymDec _ dtipo) =  find snombre st
+        
+checkAsig::Token -> SymTable -> Instruccion -> Bool
+checkAsig nombre st (InstrAsignacion identificador _)
+    = if nombre == identificador then
+        True
+      else
+        error $ "Variable no inicializada."
+      where
+        snombre = show nombre
+        Just (SymInstr _) = find snombre st
+        
+getTypeOper::Oper->Tipo
+getTypeOper oper = case oper of
+                 OperAritmetico -> TInteger
+                 OperBooleano   -> TBoolean
+                 OperCanvas     -> TCanvas
+     
+getTypeDec :: SymTable -> Symbol -> Tipo
+getTypeDec st (SymDec [token] tipo)
+    = tipo
+     
+getTypeExpr :: SymTable -> Exp -> Tipo
+getTypeExpr st (ExpBinaria exp1 oper exp2) 
+    = if getTypeExpr st exp1 /= getTypeOper oper || getTypeExpr st exp2 /= getTypeOper oper then
+        error "Error de tipo"
+      else 
+        getTypeOper oper
--- SymTable.hs
+++ SymTable.hs
@@ -1,4 +1,4 @@
---  Module SymTable
+--  Module SymSymTable
 --  Universidad Simón Bolívar
 --  Laboratorio de Traductores e Interpretadores (CI-3725)
 --  Proyecto 3 - Análisis de Contexto e Intérprete
@@ -6,7 +6,7 @@
 --                    Maria Gracia Hidalgo  03-36048
 
 module SymTable(
-  Table(..),
+  SymTable(..),
   Symbol(..),
   Tipo(..),
   Value(..),
@@ -20,18 +20,18 @@ import qualified Data.Map as Map
 import Language.AsGArD.Lexer.Token
 import Language.AsGArD.Parser.AST
 
-data Table = Table (Maybe Table) (Map.Map String Symbol)
+data SymTable = SymTable (Maybe SymTable) (Map.Map String Symbol)
               deriving (Eq, Show)
 
 
-data Symbol = SymDec [String] Tipo --FIXME
-            | Instruccion
+data Symbol = SymDec [Token] Tipo --FIXME
+            | SymInstr Instruccion
             deriving (Eq, Show)
 
 -- Argumentos que una instruccion puede tener.
 data SymArgs = Tipo
              | Token
-             | String Tipo [(Token,Tipo)]
+             | String Tipo
              deriving (Eq, Show)
 
 -- Valores que una variable Symbol puede tomar.
@@ -43,47 +43,47 @@ data Value = Numero Integer
 {-
  La funcion isMember retorna un booleano si un identificador está contenido en la tabla de símbolos.
 -}
-isMember :: String -> Table -> Bool
-isMember a (Table Nothing x) = if Map.member a x then True else False
-isMember a (Table (Just s) x) = if Map.member a x then True else isMember a s
+isMember :: String -> SymTable -> Bool
+isMember a (SymTable Nothing x) = if Map.member a x then True else False
+isMember a (SymTable (Just s) x) = if Map.member a x then True else isMember a s
 
 {-
  La funcion find retorna el símbolo asociado a un identificador en la tabla de símbolos. Si no se
  encuentra, nada es retornado.
 -}
-find :: String -> Table -> Maybe Symbol
-find a (Table Nothing x) = if Map.member a x then Map.lookup a x else Nothing
-find a (Table (Just s) x) = if Map.member a x then Map.lookup a x else find a s
+find :: String -> SymTable -> Maybe Symbol
+find a (SymTable Nothing x) = if Map.member a x then Map.lookup a x else Nothing
+find a (SymTable (Just s) x) = if Map.member a x then Map.lookup a x else find a s
 
 {-
  La funcion insert, inserta un nuevo símbolo, asociado a un identificador, en la tabla.
 -}
-insert :: String -> Symbol -> Table -> Table
-insert a b c@(Table Nothing x)
+insert :: String -> Symbol -> SymTable -> SymTable
+insert a b c@(SymTable Nothing x)
   = if Map.member a x
     then
       error $ "La variable o instruccion '"++a++"' fue declarada anteriormente."
     else
-      Table Nothing (Map.insert a b x)
-insert a b c@(Table (Just s) x)
+      SymTable Nothing (Map.insert a b x)
+insert a b c@(SymTable (Just s) x)
   = if Map.member a x
     then
       error $ "La variable o funcion '"++a++"' fue declarada anteriormente."
-    else Table (Just s) (Map.insert a b x)
+    else SymTable (Just s) (Map.insert a b x)
 
 {-
  La funcion replace, reemplaza el símbolo asociado con un identificador en la tabla. Si no existe,
  retorna la tabla sin modificaciones. Si existe, retorna la tabla con el símbolo asociado con el
  identificador reemplazado.
 -}
-replace :: String -> Symbol -> Table -> Table
+replace :: String -> Symbol -> SymTable -> SymTable
 replace a b c = if isMember a c then findSpot a b c else c
     where
-      findSpot a' b' (Table Nothing x) =
-          Table Nothing (Map.alter f a' x)
+      findSpot a' b' (SymTable Nothing x) =
+          SymTable Nothing (Map.alter f a' x)
               where f _ = Just b'
-      findSpot a' b' (Table (Just s) x) =
+      findSpot a' b' (SymTable (Just s) x) =
           if Map.member a x
-          then Table (Just s) (Map.alter f a x)
+          then SymTable (Just s) (Map.alter f a x)
           else findSpot a' b' s
               where f _ = Just b'
